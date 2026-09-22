c28a7606f25f2e07c7f9b335caf68ac3f4e24ad0
remotejobsreferrals-rgb <remotejobsreferrals@gmail.com>
2026-09-22 14:18:44 +0530

    Fix import path for create_retrieval_chain

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import chromadb
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
