a6a31250029f2eccbc527184f9b6b3fcb3c888f9
remotejobsreferrals-rgb <remotejobsreferrals@gmail.com>
2026-09-22 14:15:49 +0530

    Update import path for Chroma module

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import chromadb
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
